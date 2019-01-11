b66b8426a651f0db40ca783375582c199b6d0491
marcobrasci1986 <marco_brasci@hotmail.com>
2019-01-11 13:14:27 +0100

    Finishing Adding keyboard events to our control

--- src/app/stock-inventory/components/stock-branch/stock-branch.component.ts
+++ src/app/stock-inventory/components/stock-branch/stock-branch.component.ts
@@ -1,23 +1,30 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {FormGroup} from '@angular/forms';
 
+//Start Here Validators object for FormControls 4:46
 @Component({
   selector: 'app-stock-branch',
   styleUrls: ['./stock-branch.component.scss'],
   template: `
     <div [formGroup]="parent">
-
       <div formGroupName="store">
         <input
           type="text"
           placeholder="Branch Id"
           formControlName="branch"/>
 
+        <div class="error" *ngIf="parent.get('store.branch').hasError('required') && parent.get('store.branch').touched">
+          Branch id is required
+        </div>
         <input
           type="text"
           placeholder="Manager code"
           formControlName="code"
         />
+
+        <div class="error" *ngIf="parent.get('store.code').hasError('required') && parent.get('store.code').touched">
+          Manager id is required
+        </div>
       </div>
 
     </div>
--- src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Item, Product} from '../../models/product.interface';
 import {StockInventoryService} from '../../services/stock-inventory.service';
 import {forkJoin} from 'rxjs/observable/forkJoin';
@@ -50,6 +50,7 @@ import {forkJoin} from 'rxjs/observable/forkJoin';
 
         <pre>
           {{ form.value | json }}
+          Invalid? {{ form.invalid | json }}
         </pre>
 
       </form>
@@ -65,8 +66,8 @@ export class StockInventoryComponent implements OnInit {
 
   form = this.fb.group({
     store: this.fb.group({
-      branch: '',
-      code: ''
+      branch: ['', Validators.required],
+      code: ['', Validators.required]
     }),
     selector: this.createStock({}),
     stock: this.fb.array([])
